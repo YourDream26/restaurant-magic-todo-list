d12e25b4e387f8642c9cd77e833638f509c6fb0b
Andrey Paramonov <yourdream1375@gmail.com>
2021-05-16 19:42:17 +0300

    Implementation form logic

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,4 +1,7 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { SessionQuery } from 'src/app/state/todo.query';
 
 import { TodosService } from '../../state/todo.service';
 @Component({
@@ -13,12 +16,44 @@ export class FormComponent implements OnInit {
   public placeholderTitle = 'Task Name';
   public buttonTitle = 'Add';
 
-  constructor(private service: TodosService) { }
+  private subscription: Subscription | undefined;
 
-  public ngOnInit(): void {}
+  public existingList: Array<string> = [];
 
-  public submitHandler(event: Event): void {
-    event.preventDefault();
-    this.service.add('New Task');
+  public form: FormGroup = new FormGroup({
+    title: new FormControl('', {
+      validators: [
+        Validators.required,
+        Validators.pattern(/([a-zA-Z0-9])(a-zA-Z0-9)*/)
+      ]
+    })
+  })
+
+  constructor(private service: TodosService, private store: SessionQuery) { }
+
+  public ngOnInit(): void {
+    this.subscription = this.store.existingTitlesList$().subscribe(titles => {
+      this.existingList = titles;
+    });
+  }
+
+  public submitHandler(): void {
+    if (this.form.valid) {
+      const value = this.form.value.title.trim();
+      if (~this.existingList.indexOf(value)) {
+        alert('Task with this name already Exists!');
+      } else {
+        this.service.add(value);
+        this.form.reset();
+      }
+    } else {
+      alert('Task name cannot be empty or contain special characters!');
+    }
+  }
+
+  public ngOnDestroy(): void {
+    if (this.subscription !== undefined) {
+      this.subscription.unsubscribe();
+    }
   }
 }
--- src/app/components/form/form.module.ts
+++ src/app/components/form/form.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { FormComponent } from './form.component';
 
@@ -8,7 +9,8 @@ import { FormComponent } from './form.component';
     FormComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule
   ],
   exports: [
     FormComponent
